085a9ee1b42a09786ceb1c61b64d9539249f4d0a
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-06-21 11:34:56 +0100

    002 - Func<TResult> & Action<T, T, T>

---
+++ 002/ConsoleApp/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Action action = delegate () { Transfer(); };
+
+            //Func<string> func = delegate () {  return Withdraw(); };
+
+            //action();
+
+            //var result = func();
+
+            //TransferService<UserAccount> transferService = new TransferService<UserAccount>(delegate () { return GetUserAccount(); });
+
+            TransferService<UserAccount> transferService = new TransferService<UserAccount>(() => { return GetUserAccount(); });
+
+            await transferService.SendMoneyAsync(new UserAccount("Michel Clarke", "Sterling Bank", "038230239", 120.00), callback: async (account, response, status) => {
+
+                var result = status switch
+                {
+                    TransferStatus.APPROVED => "Approved",
+                    TransferStatus.PENDING => "Pending",
+                    TransferStatus.FAILED => "Failed",
+                    _ => "Declined",
+                };
+
+                //Giving response base on the response status and message
+                Console.WriteLine(response);
+
+                if (result.ToLower() == "pending")
+                {
+                    //TODO: Calling an external service for requery or notification
+                    await Task.Delay(2000);
+                }
+
+                //TODO: Notifying customer based on the transaction
+                Console.WriteLine("Completed!!!");
+            });
+
+            Console.ReadLine();
+        }
+
+        public static UserAccount GetUserAccount ()
+        {
+            return new UserAccount("John Doe", "Sterling Bank", "003829393", 90.00);
+        }
+
+        public static void Transfer()
+        {
+            Console.WriteLine("Transfering...");
+        }
+    }
+
+    class TransferService <T> where T : UserAccount
+    {
+        private T _Sender;
+
+        private Func<T> _GetSender;
+        private T Sender
+        {
+            get
+            {
+                if(_Sender == null)
+                {
+                   _Sender = _GetSender();
+                }
+                return _Sender;
+            }
+        }
+        public TransferService (Func<T> GetSender)
+        {
+            _Sender = null;
+            _GetSender = GetSender;
+        }
+
+        public async Task SendMoneyAsync(UserAccount beneficiaryAccount, Action<UserAccount, string, TransferStatus> callback)
+        {
+            Console.WriteLine("Processing...");
+            Console.WriteLine($"Sending {Sender.GetAmount()} to {Sender.GetBank()} - {Sender.GetAccountNumber()}");
+            await Task.Delay(5000);
+            Console.WriteLine("Sent!!!");
+            callback(beneficiaryAccount, $"Your transaction of ${beneficiaryAccount.GetAmount()} to {beneficiaryAccount.GetAccountName()} is still processing", TransferStatus.PENDING);
+        }
+    }
+
+    public class UserAccount : IUserAccount
+    {
+        private  string AccountName { get; set; }
+        private string Bank { get; set; }
+        private string AccountNumber { get; set; }
+        private double Amount { get; set; }
+
+        private UserAccount () { }
+        public UserAccount(string accountName, string bank, string accountNumber, double amount)
+        {
+            AccountName = accountName;
+            Bank = bank;
+            AccountNumber = accountNumber;
+            Amount = amount;
+        }
+
+        public string GetBank() => Bank;
+        public string GetAccountNumber() => AccountNumber;
+        public double GetAmount() => Amount;
+        public string GetAccountName() => AccountName;
+    }
+
+    public interface IUserAccount
+    {
+        public string GetBank();
+        public string GetAccountNumber();
+        public double GetAmount();
+        public string GetAccountName();
+    }
+
+    public enum TransferStatus
+    {
+        APPROVED = 1,
+        PENDING,
+        FAILED,
+        DECLINED
+    }
+}
