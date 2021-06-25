8c182128b4ea267f46ca054d6495c6564fef75ff
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-06-25 23:02:39 +0100

    Concurrent Bag -- 006

---
+++ 006/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+namespace ConcurrentBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            for (int i = 1; i <= 50; ++i)
+            {
+                bag.Add(i);
+            }
+            var task1 = Task.Factory.StartNew(() => {
+                while (bag.IsEmpty == false)
+                {
+                    int item;
+                    if (bag.TryTake(out item))
+                    {
+                        Console.WriteLine($"{item} was picked by {Task.CurrentId}");
+                    }
+                }
+            });
+            var task2 = Task.Factory.StartNew(() => {
+                while (bag.IsEmpty == false)
+                {
+                    int item;
+                    if (bag.TryTake(out item))
+                    {
+                        Console.WriteLine($"{item} was picked by {Task.CurrentId}");
+                    }
+                }
+            });
+            Task.WaitAll(task1, task2);
+            Console.WriteLine("DONE");
+        }
+
+    } 
+}
