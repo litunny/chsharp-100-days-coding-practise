f7efd2d27f1ded1d02758a98a323f5edae5f6ed6
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-06-27 23:35:56 +0100

    Updates

---
+++ 008/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<IEnumerable<string>> listOfNames = new Lazy<IEnumerable<string>>(() => new List<string> { "Michael", "Miracle", "Mika", "Mendes"}) ;
+
+            foreach(var name in listOfNames.Value)
+            {
+                Console.WriteLine($"The name: {name}");
+            }
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
