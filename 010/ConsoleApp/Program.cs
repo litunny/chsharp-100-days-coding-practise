6d9d57fe5f39b946b7f5425fdadf18c4c67c6da5
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-06-29 23:30:11 +0100

    010 - IObservable

---
+++ 010/ConsoleApp/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var provider = new StockTrader();
+            var i1 = new Investor();
+            i1.Subscribe(provider);
+            var i2 = new Investor();
+            i2.Subscribe(provider);
+
+            provider.Trade(new Stock());
+            provider.Trade(new Stock());
+            provider.Trade(null);
+            provider.End();
+        }
+    }
+
+    public class Stock
+    {
+        private string Symbol { get; set; }
+        private decimal Price { get; set; }
+    }
+
+    public class Investor : IObserver<Stock>
+    {
+        public IDisposable unsubscriber;
+        public virtual void Subscribe(IObservable<Stock> provider)
+        {
+            if (provider != null)
+            {
+                unsubscriber = provider.Subscribe(this);
+            }
+        }
+        public virtual void OnCompleted()
+        {
+            unsubscriber.Dispose();
+        }
+        public virtual void OnError(Exception e)
+        {
+        }
+        public virtual void OnNext(Stock stock)
+        {
+        }
+    }
+
+    public class StockTrader : IObservable<Stock>
+    {
+        public StockTrader()
+        {
+            observers = new List<IObserver<Stock>>();
+        }
+        private IList<IObserver<Stock>> observers;
+        public IDisposable Subscribe(IObserver<Stock> observer)
+        {
+            if (!observers.Contains(observer))
+            {
+                observers.Add(observer);
+            }
+            return new Unsubscriber(observers, observer);
+        }
+        public class Unsubscriber : IDisposable
+        {
+            private IList<IObserver<Stock>> _observers;
+            private IObserver<Stock> _observer;
+
+            public Unsubscriber(IList<IObserver<Stock>> observers, IObserver<Stock> observer)
+            {
+                _observers = observers;
+                _observer = observer;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+            private bool _disposed = false;
+            protected virtual void Dispose(bool disposing)
+            {
+                if (_disposed)
+                {
+                    return;
+                }
+                if (disposing)
+                {
+                    if (_observer != null && _observers.Contains(_observer))
+                    {
+                        _observers.Remove(_observer);
+                    }
+                }
+                _disposed = true;
+            }
+        }
+        public void Trade(Stock stock)
+        {
+            foreach (var observer in observers)
+            {
+                if (stock == null)
+                {
+                    observer.OnError(new ArgumentNullException());
+                }
+                observer.OnNext(stock);
+            }
+        }
+        public void End()
+        {
+            foreach (var observer in observers.ToArray())
+            {
+                observer.OnCompleted();
+            }
+            observers.Clear();
+        }
+    }
+}
