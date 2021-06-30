7b7598af70363e685b25e121afbab42e25e28d47
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-06-30 23:33:38 +0100

    011 - Tuple Types

---
+++ 011/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Without variable
+            (double, int) values = (10.0, 10);
+
+            Console.WriteLine($"Item One : {values.Item1} | Item Two : {values.Item2}");
+
+            //With variable
+            (string name, int age) person = ("Emmanuel", 90);
+
+            Console.WriteLine($"Name : {person.name} | Age : {person.age}");
+
+
+            //Using Tuple Fields
+            var name = (FirstName: "Emmanuel", LastName: "Osinnowo");
+
+            Console.WriteLine($"First Name : {name.FirstName} | Last Name : {name.LastName}");
+        }
+    }
+}
