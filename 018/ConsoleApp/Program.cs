4d7a4ad6ee428c2b13e560d1dbb1cd6e3be3299c
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-07-07 23:12:52 +0100

    Updates

---
+++ 018/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new();
+
+            Person person = new("Emmanuel", "Osinnowo");
+
+            Console.WriteLine(person.FirstName);
+
+            Person person2 = person with { FirstName = "Michael" };
+
+            Console.WriteLine(person2.FirstName);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public record Person(string FirstName, string LastName);
+}
