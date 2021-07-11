6ac5e1f5a7953b35ea94f310088e51ea8a34be30
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-07-11 23:10:04 +0100

    023 - Self Invoking Method

---
+++ 023/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Func<Dictionary<Int32, String>>(() =>
+            {
+                return new Dictionary<Int32, String>
+                {
+                    { 0, "Foo" },
+                    { 1, "Bar" },
+                    { 2, "..." }
+                };
+           })();
+
+           Func<String, String> Shorten = s => s.Length > 100 ? s.Substring(0, 100) + "&hellip;" : s;
+        }
+    }
+}
