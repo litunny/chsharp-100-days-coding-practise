d0db95850de50e931bfac60c34006d3c7408398c
Osinnowo Emmanuel Itunu <liquidcoding2009@gmail.com>
2021-07-13 23:29:04 +0100

    Updates

---
+++ 024/ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Bruce");
+            names.Add("Alfred");
+            names.Add("Tim");
+            names.Add("Richard");
+
+            // Display the contents of the list using the Print method.
+            names.ForEach(Print);
+
+            // The following demonstrates the anonymous method feature of C#
+            // to display the contents of the list to the console.
+            names.ForEach(delegate (String name)
+            {
+                Console.WriteLine(name);
+            });
+
+            void Print(string s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
